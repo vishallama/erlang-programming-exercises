930a87282784ce8a754a5fe0cb551a9c1a35d47f
Vishal Lama <vishal.lama@gmail.com>
2017-03-19 12:57:37 -0600

    Do Ex 3-2: Creating Lists - Implement create/1

--- src/sequential.erl
+++ src/sequential.erl
@@ -10,7 +10,7 @@
 -author("vishal").
 
 %% API
--export([sum/1, sum/2]).
+-export([sum/1, sum/2, create/1]).
 
 % Ex 3-1: Evaluating Expressions
 sum(0) ->
@@ -22,3 +22,11 @@ sum(N, N) ->
   N;
 sum(N, M) when N =< M ->
   N + sum(N + 1, M).
+
+% Ex 3-2: Creating Lists
+create(N) when N > 0 -> create(N, []).
+
+create(0, List) ->
+  List;
+create(N, List) ->
+  create(N - 1, [N | List]).
