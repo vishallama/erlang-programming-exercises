6b428444bd0ec8627ec5aaf5188d4893ba0d966e
Vishal Lama <vishal.lama@gmail.com>
2017-03-19 11:55:44 -0600

    Do Ex 3-1: Evaluating Expressions - Implement sum/2

--- src/sequential.erl
+++ src/sequential.erl
@@ -10,10 +10,15 @@
 -author("vishal").
 
 %% API
--export([sum/1]).
+-export([sum/1, sum/2]).
 
 % Ex 3-1: Evaluating Expressions
 sum(0) ->
   0;
 sum(N) when N > 0 ->
   N + sum(N - 1).
+
+sum(N, N) ->
+  N;
+sum(N, M) when N =< M ->
+  N + sum(N + 1, M).
